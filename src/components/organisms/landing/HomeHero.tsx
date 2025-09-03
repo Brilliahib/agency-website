134322de1d29856844466f4f057721bf345795a1
brilliahib <brilliahib21@gmail.com>
2025-09-03 22:33:39 +0700

    feat: create a motion on landing

--- src/components/organisms/landing/HomeHero.tsx
+++ src/components/organisms/landing/HomeHero.tsx
@@ -2,10 +2,28 @@
 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import type { Variants } from "framer-motion";
 
 export default function HomeHero() {
+  const container: Variants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const item: Variants = {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0 },
+  };
+
   return (
     <section className="w-full justify-center items-center h-screen relative overflow-hidden">
+      {/* background blur */}
       <div className="absolute inset-0 -z-10">
         <div className="absolute md:flex hidden top-20 left-10 w-72 h-72 bg-primary/40 rounded-full blur-[120px]" />
         <div className="absolute md:flex hidden bottom-80 right-20 w-96 h-96 bg-primary/30 rounded-full blur-[150px]" />
@@ -14,19 +32,34 @@ export default function HomeHero() {
 
       <div className="grid md:grid-cols-2 gap-8 h-full md:pl-[12%] md:text-left text-center">
         <div className="flex items-center h-full">
-          <div className="md:max-w-3xl md:space-y-8 space-y-6 md:px-0 px-4">
-            <h1 className="text-4xl md:text-6xl font-bold">
+          <motion.div
+            className="md:max-w-3xl md:space-y-8 space-y-6 md:px-0 px-4"
+            variants={container}
+            initial="hidden"
+            animate="show"
+          >
+            <motion.h1
+              className="text-4xl md:text-6xl font-bold"
+              variants={item}
+            >
               Your One-Stop Solution for Digital Needs
-            </h1>
-            <p className="md:text-lg leading-relaxed text-muted-foreground">
+            </motion.h1>
+
+            <motion.p
+              className="md:text-lg leading-relaxed text-muted-foreground"
+              variants={item}
+            >
               Kami menyediakan layanan digital mulai dari web app, mobile app,
               hingga desain kreatif seperti logo, poster, dan UI/UX untuk
               membantu brand Anda tampil profesional dan mudah diingat.
-            </p>
-            <Button className="rounded-md" size={"lg"}>
-              Lihat Selengkapnya
-            </Button>
-          </div>
+            </motion.p>
+
+            <motion.div variants={item}>
+              <Button className="rounded-md" size={"lg"}>
+                Lihat Selengkapnya
+              </Button>
+            </motion.div>
+          </motion.div>
         </div>
       </div>
 
