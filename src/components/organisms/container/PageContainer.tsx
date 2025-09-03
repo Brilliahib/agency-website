0436a9abfc1cce98e6a8c1ce76c857718d37ab60
brilliahib <brilliahib21@gmail.com>
2025-09-04 01:40:49 +0700

    fix: padding on page container

--- src/components/organisms/container/PageContainer.tsx
+++ src/components/organisms/container/PageContainer.tsx
@@ -1,5 +1,9 @@
 import { PropsWithChildren } from "react";
 
 export default function PageContainer({ children }: PropsWithChildren) {
-  return <section className="pad-x-xl py-30 space-y-12">{children}</section>;
+  return (
+    <section className="pad-x-xl pb-30 md:pt-38 pt-35 space-y-12">
+      {children}
+    </section>
+  );
 }
